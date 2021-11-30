5b922bf3fc5c49f82ef479968c5371b606d0c86a
mohitpadhye <mohitmpadhye@gmail.com>
2021-11-30 00:10:35 -0600

    first commit

--- src/useAuth.js
+++ src/useAuth.js
@@ -8,7 +8,7 @@ export default function useAuth(code) {
 
   useEffect(() => {
     axios
-      .post("https://soundhouseserver.herokuapp.com/login", {
+      .post("http://localhost:3001/login", {
         code,
       })
       .then(res => {
@@ -26,7 +26,7 @@ export default function useAuth(code) {
     if (!refreshToken || !expiresIn) return
     const interval = setInterval(() => {
       axios
-        .post("https://soundhouseserver.herokuapp.com/refresh", {
+        .post("http://localhost:3001/refresh", {
           refreshToken,
         })
         .then(res => {
